0b0de9baca93ecd9b71611de6a2c4a54ae1b35ae
atella123 <upolushkinm@mail.ru>
2026-04-22 11:51:31 +0500

    fix circular dependency

--- backend/src/api/dependencies.py
+++ backend/src/api/dependencies.py
@@ -1,7 +1,7 @@
-from typing import Annotated, Any
+from typing import Annotated, Any, Literal
 
 import aiohttp
-from fastapi import Depends, Request
+from fastapi import Depends, HTTPException, Request, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database import get_session
@@ -17,6 +17,21 @@ def get_location_data(request: Request) -> list[dict[str, Any]]:
     return request.app.state.location_data
 
 
-type DBSession = Annotated[AsyncSession, Depends(get_session)]
 type Token = Annotated[TokenPayload, Depends(get_request_user)]
+
+
+def validate_user_id_path(
+    user_id: int | Literal["me"],
+    token: Token,
+) -> None:
+    if user_id == "me":
+        return
+
+    if user_id != token.user_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Access denied"
+        )
+
+
+type DBSession = Annotated[AsyncSession, Depends(get_session)]
 type HTTPClient = Annotated[aiohttp.ClientSession, Depends(get_http_session)]
