b2917d42fec4fae2a4ba9a5ffa30ded2316aa6c8
atella123 <upolushkinm@mail.ru>
2026-04-25 20:09:33 +0500

    disable rate limiting in dev mode

--- backend/src/core/rate_limiter.py
+++ backend/src/core/rate_limiter.py
@@ -3,8 +3,10 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
 
+from core.config import settings
+
 # NOTE: also use user_id as key_func param?
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(key_func=get_remote_address, enabled=not settings.DEV_MODE)
 
 
 # https://github.com/muhannad-hash/slowapi/blob/fix/issue-188/slowapi/extension.py
