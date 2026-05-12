321876973576ab5f8dded4f6a688647b9a018b80
atella123 <upolushkinm@mail.ru>
2026-05-12 16:32:16 +0500

    fix column name

--- backend/src/service/export.py
+++ backend/src/service/export.py
@@ -25,7 +25,7 @@ COLUMN_MAPPING: dict[str, str] = {
     "longitude": "Longitude",
     "verbatimcoordinates": "Verbatim Coordinates",
     "coordinate_uncertainty": "Coordinate Uncertainty (m)",
-    "georeferencedby": "Geo Referenced By",
+    "georef_source": "Geo Referenced By",
     "location_remarks": "Location Remarks",
     "verbatim_date": "Date",
     "date_precision": "Date Precision",
