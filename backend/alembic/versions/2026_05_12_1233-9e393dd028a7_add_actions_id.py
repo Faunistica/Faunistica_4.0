6933557233934b295b8a130a8cf8b49f762f8b17
atella123 <upolushkinm@mail.ru>
2026-05-15 21:53:09 +0500

    fix alembic branching

--- backend/alembic/versions/2026_05_12_1233-9e393dd028a7_add_actions_id.py
+++ backend/alembic/versions/2026_05_12_1233-9e393dd028a7_add_actions_id.py
@@ -1,7 +1,7 @@
 """add actions.id
 
 Revision ID: 9e393dd028a7
-Revises: 8b5c5cd24625
+Revises: afe1e469c3ca
 Create Date: 2026-05-12 12:33:58.329796
 
 """
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "9e393dd028a7"
-down_revision: str | None = "8b5c5cd24625"
+down_revision: str | None = "afe1e469c3ca"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
