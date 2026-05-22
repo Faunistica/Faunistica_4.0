bfe3f81ef321b26f7175abd5f78e606ff468c1ff
atella123 <upolushkinm@mail.ru>
2026-05-22 19:31:17 +0500

    backend: get publication by id second draft

--- backend/src/api/publications/current.py
+++ backend/src/api/publications/current.py
@@ -3,19 +3,12 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Query
 
 from core.dependencies import TokenUser
+from core.exceptions import PublicationNotFoundError
 from core.security import get_jwt_user
 from schema.common import Publication
 from service.publications import PublicationService
 
 router = APIRouter(prefix="/publications")
-
-
-@router.get("/{publ_id}", dependencies=[Depends(get_jwt_user)])
-async def get(
-    publ_id: int,
-    pub_service: Annotated[PublicationService, Depends()],
-) -> Publication | None:
-    return await pub_service.get(publ_id)
 
 
 @router.get("/current")
@@ -27,3 +20,16 @@ async def list_publications(
     ] = False,
 ) -> list[Publication]:
     return await pub_service.get_current(user_id=token.user_id, with_queue=list_all)
+
+
+@router.get("/{publ_id}", dependencies=[Depends(get_jwt_user)])
+async def get(
+    publ_id: int,
+    pub_service: Annotated[PublicationService, Depends()],
+) -> Publication:
+    publication = await pub_service.get(publ_id)
+
+    if publication:
+        return publication
+
+    raise PublicationNotFoundError(publ_id)
